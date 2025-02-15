14339325b84c89add687375d29a05b8d0d8d865a
Abdarrahmane-NEINE <kaberneine10@gmail.com>
2025-02-15 21:11:41 +0100

    refactor generate_report function

--- backend/app/services/llm_client.py
+++ backend/app/services/llm_client.py
@@ -8,56 +8,31 @@ import os
 import time
 # get prompt messages
 root: Path = Path(".").resolve()
-cs_prompt_file: Path = root / "prompts/stage_01/stage01_latest.md"
-fs_prompt_file: Path = root / "prompts/stage_02/stage02_latest.md"
-
 
 load_dotenv()
 api_key: str | None = settings.LLM_API_KEY
 model = settings.LLM_MISTRAL_MODEL
 
-current_dir = os.path.dirname(__file__)
-root_dir = os.path.abspath(os.path.join(current_dir, "../../.."))
-
-# root: Path = Path(".").resolve()
-# cs_prompt_file: Path = root / "prompts/stage_01/stage01_latest.md"
-# fs_prompt_file: Path = root / "prompts/stage_02/stage02_latest.md"
-
-# mock_patient_file: Path = root / "data/patient_001.txt"
 current_file: Path = Path(__file__).resolve()
 # Navigate up three levels to reach the project root
 project_root: Path = current_file.parents[3]
 
-cs_prompt_file: Path = project_root / "prompts" / "stage_01" / "stage01_latest.md"
-fs_prompt_file: Path = project_root / "prompts" / "stage_02" / "stage02_latest.md"
-mock_patient_file: Path = project_root / "data" / "patient_001.txt"
+COMPREHENSIVE_PROMPT_FILE: Path = project_root / "prompts" / "stage_01" / "stage01_latest.md"
+FINAL_SUMMARY_PROMPT_FILE: Path = project_root / "prompts" / "stage_02" / "stage02_latest.md"
+MOCK_PATIENT_FILE: Path = project_root / "data" / "patient_001.txt"
 
 
-def call_llm(prompt: str, data: str) -> str:
+def call_llm(prompt: str) -> str:
     """
     Calls the Mistral LLM with the provided prompt and data.
     Returns the generated summary.
     """
-    pass
-    # client = Mistral(api_key=settings.LLM_API_KEY)
-    # chat_response: ChatCompletionResponse = client.chat.complete(
-    #     model=model,
-    #     messages=[{"role": "user", "content": cs_prompt}],
-    # )
-    # payload = {
-    #         "prompt": prompt,
-    #         "data": data
-    #     }
-    # headers = {
-    #     "Authorization": f"Bearer {settings.LLM_API_KEY}",
-    #     "Content-Type": "application/json"
-    # }
-    # response = requests.post(settings.LLM_ENDPOINT, json=payload, headers=headers)
-    # if response.status_code == 200:
-    #     result = response.json()
-    #     return result.get("output", "")
-    # else:
-    #     raise Exception(f"LLM request failed with status code {response.status_code}")
+    client = Mistral(api_key=settings.LLM_API_KEY)
+    chat_response: ChatCompletionResponse = client.chat.complete(
+        model=settings.LLM_MISTRAL_MODEL,
+        messages=[{"role": "user", "content": prompt}],
+    )
+    return chat_response.choices[0].message.content
     
 
 def generate_report(patient_id: str) -> Tuple[str, str]:
@@ -65,44 +40,36 @@ def generate_report(patient_id: str) -> Tuple[str, str]:
     Mock function to simulate generating a patient's report.
     Returns a comprehensive report and a final report.
     """
-    client = Mistral(api_key=api_key)
-    # print current path
-    # print(f"Current path: {os.getcwd()}")
-
     # get files for patient
-    if mock_patient_file.is_file() is False:
-        raise FileNotFoundError(f"File {mock_patient_file} not found.")
-    content: str = mock_patient_file.read_text(encoding="utf-8")
+    if MOCK_PATIENT_FILE.is_file() is False:
+        raise FileNotFoundError(f"File {MOCK_PATIENT_FILE} not found.")
+    content: str = MOCK_PATIENT_FILE.read_text(encoding="utf-8")
 
     # create Comprehensive Medical History Summary
-    if cs_prompt_file.is_file() is False:
-        raise FileNotFoundError(f"File {cs_prompt_file} not found.")
-    cs_prompt: str = cs_prompt_file.read_text(encoding="utf-8").format(content=content)
-    chat_response: ChatCompletionResponse = client.chat.complete(
-        model=model,
-        messages=[{"role": "user", "content": cs_prompt}],
-    )
-    com_report: str = chat_response.choices[0].message.content
+    if COMPREHENSIVE_PROMPT_FILE.is_file() is False:
+        raise FileNotFoundError(f"File {COMPREHENSIVE_PROMPT_FILE} not found.")
+    
+    comprehensive_prompt: str = COMPREHENSIVE_PROMPT_FILE.read_text(encoding="utf-8").format(content=content)
+    comprehensive_report: str = call_llm(comprehensive_prompt)
     # save to temp file with timestamp
-    timestamp: str = time.strftime("%Y%m%d_%H%M%S")
-    report_file_cs: Path = root / f"../app/reports/{patient_id}_cs_{timestamp}.md"
-    report_file_cs.write_text(data=com_report, encoding="utf-8")
+    save_report("cs", comprehensive_report, patient_id, root)
 
     # create Final Report
-    if fs_prompt_file.is_file() is False:
-        raise FileNotFoundError(f"File {fs_prompt_file} not found.")
-    fs_prompt: str = fs_prompt_file.read_text(encoding="utf-8").format(content=com_report)
-    chat_response = client.chat.complete(
-        model=model,
-        messages=[{"role": "user", "content": fs_prompt}],
-    )
-    final_report: str = chat_response.choices[0].message.content
+    if FINAL_SUMMARY_PROMPT_FILE.is_file() is False:
+        raise FileNotFoundError(f"File {FINAL_SUMMARY_PROMPT_FILE} not found.")
+    final_summary_prompt: str = FINAL_SUMMARY_PROMPT_FILE.read_text(encoding="utf-8").format(content=comprehensive_report)
+
+    final_report: str = call_llm(final_summary_prompt)
     # save to temp file with timestamp
-    timestamp: str = time.strftime("%Y%m%d_%H%M%S")
-    report_file_fs: Path = root / f"../app/reports/{patient_id}_fs_{timestamp}.md"
-    report_file_fs.write_text(data=final_report, encoding="utf-8")
+    report_file_fs = save_report("fs", final_report, patient_id, root)
 
     # print(f"Comprehensive Report saved to: {report_file_cs}")
     print(f"Final Report saved to: {report_file_fs}")
 
-    return com_report, final_report
+    return comprehensive_report, final_report
+
+def save_report(report_type: str, report_content: str, patient_id: str, root: Path) -> None:
+    timestamp: str = time.strftime("%Y%m%d_%H%M%S")
+    file_path: Path = root / f"../app/reports/{patient_id}_{report_type}_{timestamp}.md"
+    file_path.write_text(data=report_content, encoding="utf-8")
+    return file_path
